c656aec321c48354c82872ba71418f4dca974858
Felipe Polo Ruiz <felipe.polo@gmail.com>
2020-06-12 14:09:40 +0200

    Add method labels via console
    Co-authored-by: Carlos Jiménez <betisman@gmail.com>

--- advanced/merging-arrays/index.js
+++ advanced/merging-arrays/index.js
@@ -15,11 +15,18 @@ const method4 = (list1, list2) => ([...list1, ...list2]);
 const array1 = [1, 2, 3];
 const array2 = [4, 5, 6];
 
+console.log('Method 1:');
 console.log(method1(array1, array2));
 console.log(`Non mutated! [ ${array1} ]`);
+
+console.log('\nMethod 2:');
 console.log(method2(array1, array2));
 console.log(`Array 1 has been mutated! [ ${array1} ]`); // mutated!
-console.log(method3([ 1, 2, 3 ], array2)); // new instance on the fly for array1
-const newArray1 = [ 1, 2, 3 ];
+
+console.log('\nMethod 3:');
+console.log(method3([1, 2, 3], array2)); // new instance on the fly for array1
+
+console.log('\nMethod 4:');
+const newArray1 = [1, 2, 3];
 console.log(method4(newArray1, array2));
 console.log(`Non mutated! [ ${newArray1} ]`);
