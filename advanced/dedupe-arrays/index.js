001d3eba8577c37af9f5a8061a98725ee2e3ae69
Carlos Jiménez <betisman@gmail.com>
2020-07-03 16:00:22 +0200

    Add dedupe arrays pill (#4)
    * Add dedupe arrays pill
    
    * Update readme and create an explained step by step runner for dedupe array
    
    * fix typo
    
    * last minute formatting update
    
    * update readme

---
+++ advanced/dedupe-arrays/index.js
@@ -0,0 +1,14 @@
+const originalArray = ['🇪🇸', '🏁', '🇪🇺', '🇪🇸', '🇪🇸', '🇮🇨', '🇮🇨', '🏳️‍🌈', '🇮🇨', '🇮🇨', '🇮🇨', '🇪🇺'];
+
+const reduceMethod = array => array.reduce((acc, flag) => acc.includes(flag) ? acc : [...acc, flag], []);
+
+const filterMethod = array => array.filter((flag, index) => array.indexOf(flag) === index);
+
+const setMethod = array => [...new Set(array)];
+
+console.log(reduceMethod(originalArray));
+console.log(filterMethod(originalArray));
+console.log(setMethod(originalArray));
+
+// for the benchmark
+module.exports = ({ reduceMethod, filterMethod, setMethod });
