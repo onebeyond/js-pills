b69636e07034b8618ae07f225d1aa4afc0a3d684
Giorgio Grassini <giorgiograssini@gmail.com>
2021-02-19 09:25:15 +0100

    changed typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,42 @@
-import Typography from 'typography';
-import './global.css';
+import Typography from "typography";
+import Wordpress2016 from "typography-theme-wordpress-2016";
+import "./global.css";
 
-const typography = new Typography();
+Wordpress2016.overrideThemeStyles = () => {
+  return {
+    "a.gatsby-resp-image-link": {
+      boxShadow: `none`,
+    },
+    a: {
+      color: "var(--textLink)",
+      textDecoration: "none"
+    },
+    // gatsby-remark-autolink-headers - don't underline when hidden
+    "a.anchor": {
+      boxShadow: "none",
+    },
+    // gatsby-remark-autolink-headers - use theme colours for the link icon
+    'a.anchor svg[aria-hidden="true"]': {
+      stroke: "var(--textLink)",
+    },
+    h3: {
+      fontFamily: "'Roboto Mono', sans-serif",
+      fontSize: "1.5rem",
+    },
+    p: {
+      fontFamily: "'Roboto Mono Medium', sans-serif",
+      fontSize: "1.125rem",
+    },
+    hr: {
+      background: "var(--hr)",
+    },
+    small: {
+      color: "var(--textNormal)",
+    },
+  };
+};
+
+const typography = new Typography(Wordpress2016);
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
