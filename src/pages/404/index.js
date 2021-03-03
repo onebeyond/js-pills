ac86e233db7e71b1d75138336709ba02db91f319
Giorgio Grassini <giorgiograssini@gmail.com>
2021-03-03 17:35:06 +0100

    404 styled

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const NotFoundPage = () => {
-  return <div>Pill not found</div>;
-};
+import Layout from '../../layouts/mainLayout';
+import SEO from '../../components/seo';
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title='Pill not found' />
+    <div>Pill not found</div>
+  </Layout>
+);
 
 export default NotFoundPage;
