e616220aab96543afe2db943516b12916336e03a
Giorgio Grassini <giorgiograssini@gmail.com>
2021-03-04 12:33:07 +0100

    RSS (#58)
    * RSS

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,3 @@
-const { createFilePath } = require(`gatsby-source-filesystem`);
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions;
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode });
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    });
-  }
-};
-
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions;
 
